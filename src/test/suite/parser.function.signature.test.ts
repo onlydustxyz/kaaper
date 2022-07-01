59d13de251b581894be0662d1278f56336eef83f
jedi <amajidsinar@gmail.com>
2022-07-01 23:19:42 +0100

    test(function.signature): refactor

--- src/test/suite/parser.function.signature.test.ts
+++ src/test/suite/parser.function.signature.test.ts
@@ -27,35 +27,36 @@ suite("scope: constructor test", () => {
     assert.equal("constructor", functionName, "failed to get attribute name");
   });
 
-  // test("get implicit args", () => {
-  //   const pathFile = path.resolve(
-  //     __dirname,
-  //     "../../../test_assets/ERC20.cairo"
-  //   );
-  //   let constructorParser = new CairoParser(pathFile, "constructor");
-  //   const attributeScope = constructorParser.parseAttribute();
-  //   const implicitArgs = constructorParser.getImplicitArgs(attributeScope);
-  //   const targetArgs = new Map();
-  //   targetArgs.set("syscall_ptr", "felt*");
-  //   targetArgs.set("pedersen_ptr", "HashBuiltin*");
-  //   targetArgs.set("range_check_ptr", "");
+  test("get implicit args", () => {
+    const pathFile = path.resolve(
+      __dirname,
+      "../../../test_assets/ERC20.cairo"
+    );
+    let constructorParser = new CairoParser(pathFile, "constructor");
+    const constructorText = constructorParser.parseFunctionScope();
+    const functionSignatureParser = new FunctionSignatureParser();
+    const implicitArgs = functionSignatureParser.getImplicitArgs(constructorText)
+    const targetArgs = new Map();
+    targetArgs.set("syscall_ptr", "felt*");
+    targetArgs.set("pedersen_ptr", "HashBuiltin*");
+    targetArgs.set("range_check_ptr", "");
 
-  //   // TODO: refactor this, somehow the two Maps are not equal but if we unpack it they are equal
-  //   const targetArgsIter = targetArgs.values();
-  //   const implicitArgsIter = implicitArgs.values();
-  //   assert.equal(
-  //     targetArgs.size,
-  //     implicitArgs.size,
-  //     "failed to get implicit args"
-  //   );
-  //   for (let i = 0; i < targetArgs.size; i++) {
-  //     assert.equal(
-  //       targetArgsIter.next().value,
-  //       implicitArgsIter.next().value,
-  //       "failed to get explicit args"
-  //     );
-  //   }
-  // });
+    // TODO: refactor this, somehow the two Maps are not equal but if we unpack it they are equal
+    const targetArgsIter = targetArgs.values();
+    const implicitArgsIter = implicitArgs.values();
+    assert.equal(
+      targetArgs.size,
+      implicitArgs.size,
+      "failed to get implicit args"
+    );
+    for (let i = 0; i < targetArgs.size; i++) {
+      assert.equal(
+        targetArgsIter.next().value,
+        implicitArgsIter.next().value,
+        "failed to get explicit args"
+      );
+    }
+  });
 
   // test("get explicit args", () => {
   //   const pathFile = path.resolve(
