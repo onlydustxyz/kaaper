32b1f8c301f4ef13c7445354dade60935292717b
jedi <amajidsinar@gmail.com>
2022-07-01 23:22:40 +0100

    test(function.signature): refactor explicit args

--- src/test/suite/parser.function.signature.test.ts
+++ src/test/suite/parser.function.signature.test.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 import FunctionSignatureParser from "../../lib/parser/functionsignature";
 import CairoParser from "../../lib/main";
 
-suite("scope: constructor test", () => {
+suite("scope: constructor", () => {
   test("get attribute name", () => {
     const pathFile = path.resolve(
       __dirname,
@@ -58,34 +58,35 @@ suite("scope: constructor test", () => {
     }
   });
 
-  // test("get explicit args", () => {
-  //   const pathFile = path.resolve(
-  //     __dirname,
-  //     "../../../test_assets/ERC20.cairo"
-  //   );
-  //   let constructorParser = new CairoParser(pathFile, "constructor");
-  //   const attributeScope = constructorParser.parseAttribute();
-  //   const explicitArgs = constructorParser.getExplicitArgs(attributeScope);
-  //   const targetArgs = new Map();
-  //   targetArgs.set("name", "felt");
-  //   targetArgs.set("symbol", "felt");
-  //   targetArgs.set("decimals", "Uint256");
-  //   targetArgs.set("initial_supply", "Uint256");
-  //   targetArgs.set("recipient", "felt");
-  //   // TODO: refactor this, somehow the two Maps are not equal but if we unpack it they are equal
-  //   const targetArgsIter = targetArgs.values();
-  //   const explicitArgsIter = explicitArgs.values();
-  //   assert.equal(
-  //     targetArgs.size,
-  //     explicitArgs.size,
-  //     "failed to get implicit args"
-  //   );
-  //   for (let i = 0; i < targetArgs.size; i++) {
-  //     assert.equal(
-  //       targetArgsIter.next().value,
-  //       explicitArgsIter.next().value,
-  //       "failed to get explicit args"
-  //     );
-  //   }
-  // });
+  test("get explicit args", () => {
+    const pathFile = path.resolve(
+      __dirname,
+      "../../../test_assets/ERC20.cairo"
+    );
+    let constructorParser = new CairoParser(pathFile, "constructor");
+    const constructorText = constructorParser.parseFunctionScope();
+    const functionSignatureParser = new FunctionSignatureParser();
+    const explicitArgs = functionSignatureParser.getExplicitArgs(constructorText)
+    const targetArgs = new Map();
+    targetArgs.set("name", "felt");
+    targetArgs.set("symbol", "felt");
+    targetArgs.set("decimals", "Uint256");
+    targetArgs.set("initial_supply", "Uint256");
+    targetArgs.set("recipient", "felt");
+    // TODO: refactor this, somehow the two Maps are not equal but if we unpack it they are equal
+    const targetArgsIter = targetArgs.values();
+    const explicitArgsIter = explicitArgs.values();
+    assert.equal(
+      targetArgs.size,
+      explicitArgs.size,
+      "failed to get implicit args"
+    );
+    for (let i = 0; i < targetArgs.size; i++) {
+      assert.equal(
+        targetArgsIter.next().value,
+        explicitArgsIter.next().value,
+        "failed to get explicit args"
+      );
+    }
+  });
 });
