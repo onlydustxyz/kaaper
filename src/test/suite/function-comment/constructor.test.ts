61710340cc698e9acbf7d1323030c255cdb86acd
jedi <amajidsinar@gmail.com>
2022-07-02 00:31:07 +0100

    test(function-comment): refactor explicit args #6

--- src/test/suite/function-comment/constructor.test.ts
+++ src/test/suite/function-comment/constructor.test.ts
@@ -93,148 +93,26 @@ suite("function-comment: constructor", () => {
 
   });
 
-//   test("get explicit args comments", () => {
-//     const pathFile = path.resolve(
-//       __dirname,
-//       "../../../test_assets/ERC20.cairo"
-//     );
-//     let constructorParser = new CairoParser(pathFile, "constructor");
-//     const attributeScope = constructorParser.parseAttribute();
-//     const comments = constructorParser.getComments(attributeScope);
-//     if (comments) {
-//       assert.equal(
-//         "# Explicit args:",
-//         comments[6].trim(),
-//         "failed to get comments"
-//       );
-//       const isStartScope = constructorParser.isStartScope(
-//         comments[6],
-//         "Explicit args"
-//       );
-//       const isEndScope = constructorParser.isEndScope(
-//         comments[6],
-//         "Explicit args"
-//       );
-//       assert.equal(true, isStartScope, "failed to get comments");
-//       assert.equal(false, isEndScope, "failed to get comments");
-//     }
+  test("get explicit args comments", () => {
+    const pathFile = path.resolve(
+        __dirname,
+        "../../../../test_assets/ERC20.cairo"
+    );
 
-//     if (comments) {
-//       assert.equal(
-//         "#   name(felt): the address of the ERC20 sender",
-//         comments[7].trim(),
-//         "failed to get comments"
-//       );
-//       const isStartScope = constructorParser.isStartScope(
-//         comments[7],
-//         "Explicit args"
-//       );
-//       const isEndScope = constructorParser.isEndScope(
-//         comments[7],
-//         "Explicit args"
-//       );
-//       assert.equal(false, isStartScope, "failed to get comments");
-//       assert.equal(false, isEndScope, "failed to get comments");
-//     }
+      const constructorParser = new CairoParser(pathFile, "constructor");
+      const functionText = constructorParser.parseFunctionScope()
+      const commentText = constructorParser.parseComments(functionText);
+      const functionCommentParser = new FunctionCommentParser('Explicit args');
 
-//     if (comments) {
-//       assert.equal(
-//         "#   symbol(felt): the address of the ERC20 recipient",
-//         comments[8].trim(),
-//         "failed to get comments"
-//       );
-//       const isStartScope = constructorParser.isStartScope(
-//         comments[8],
-//         "Explicit args"
-//       );
-//       const isEndScope = constructorParser.isEndScope(
-//         comments[8],
-//         "Explicit args"
-//       );
-//       assert.equal(false, isStartScope, "failed to get comments");
-//       assert.equal(false, isEndScope, "failed to get comments");
-//     }
+      if (commentText) {
+        const line = 6;
+        assert.equal("# Explicit args:", commentText[line].trim(), `check line ${line}`);
+        const isStartScope = functionCommentParser.isStartScope(commentText[line]);
+        const isEndScope = functionCommentParser.isEndScope(commentText[line]);
+        assert.equal(true, isStartScope, `failed to get desc comment line ${line}`);
+        assert.equal(false, isEndScope, `failed to get desc comment line ${line}`);
+      }
 
-//     if (comments) {
-//       assert.equal(
-//         "#   decimals(uint256): floating point of the token",
-//         comments[9].trim(),
-//         "failed to get comments"
-//       );
-//       const isStartScope = constructorParser.isStartScope(
-//         comments[9],
-//         "Explicit args"
-//       );
-//       const isEndScope = constructorParser.isEndScope(
-//         comments[9],
-//         "Explicit args"
-//       );
-//       assert.equal(false, isStartScope, "failed to get comments");
-//       assert.equal(false, isEndScope, "failed to get comments");
-//     }
-
-//     if (comments) {
-//       assert.equal(
-//         "#   initial_supply(uint256): amount of ERC20 transfer",
-//         comments[10].trim(),
-//         "failed to get comments"
-//       );
-//       const isStartScope = constructorParser.isStartScope(
-//         comments[10],
-//         "Explicit args"
-//       );
-//       const isEndScope = constructorParser.isEndScope(
-//         comments[10],
-//         "Explicit args"
-//       );
-//       assert.equal(false, isStartScope, "failed to get comments");
-//       assert.equal(false, isEndScope, "failed to get comments");
-//     }
-
-//     if (comments) {
-//       assert.equal(
-//         "#   recipient(felt): amount of ERC20 transfer",
-//         comments[11].trim(),
-//         "failed to get comments"
-//       );
-//       const isStartScope = constructorParser.isStartScope(
-//         comments[11],
-//         "Explicit args"
-//       );
-//       const isEndScope = constructorParser.isEndScope(
-//         comments[11],
-//         "Explicit args"
-//       );
-//       assert.equal(false, isStartScope, "failed to get comments");
-//       assert.equal(false, isEndScope, "failed to get comments");
-//     }
-
-//     if (comments) {
-//       assert.equal("# Returns:", comments[12].trim(), "failed to get comments");
-//       const isStartScope = constructorParser.isStartScope(
-//         comments[12],
-//         "Explicit args"
-//       );
-//       const isEndScope = constructorParser.isEndScope(
-//         comments[12],
-//         "Explicit args"
-//       );
-//       assert.equal(false, isStartScope, "failed to get comments");
-//       assert.equal(true, isEndScope, "failed to get comments");
-//     }
-
-//     if (comments) {
-//       assert.equal("#   None", comments[13].trim(), "failed to get comments");
-//       const isStartScope = constructorParser.isStartScope(
-//         comments[13],
-//         "Explicit args"
-//       );
-//       const isEndScope = constructorParser.isEndScope(
-//         comments[13],
-//         "Explicit args"
-//       );
-//       assert.equal(false, isStartScope, "failed to get comments");
-//       assert.equal(false, isEndScope, "failed to get comments");
-//     }
-//   });
+    
+  });
 });
