79e0c91d32c7a51a5a13185e07febf2d9e6926b0
jedi <amajidsinar@gmail.com>
2022-07-02 00:32:28 +0100

    test(function-comment): refactor explicit args #7

--- src/test/suite/function-comment/constructor.test.ts
+++ src/test/suite/function-comment/constructor.test.ts
@@ -113,6 +113,15 @@ suite("function-comment: constructor", () => {
         assert.equal(false, isEndScope, `failed to get desc comment line ${line}`);
       }
 
+      if (commentText) {
+        const line = 7;
+        assert.equal("#   name(felt): the address of the ERC20 sender", commentText[line].trim(), `check line ${line}`);
+        const isStartScope = functionCommentParser.isStartScope(commentText[line]);
+        const isEndScope = functionCommentParser.isEndScope(commentText[line]);
+        assert.equal(false, isStartScope, `failed to get desc comment line ${line}`);
+        assert.equal(false, isEndScope, `failed to get desc comment line ${line}`);
+      }
+
     
   });
 });
