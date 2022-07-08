918f9b8be1d5c51d3ecf0011cfdaeb2cab9f5511
jedi <amajidsinar@gmail.com>
2022-07-08 12:18:35 +0100

    delete

--- src/test/suite/hello.world.test.ts
+++
@@ -1,9 +0,0 @@
-import * as assert from "assert";
-import HelloWorld from "../../lib/HelloWorld"
-
-suite("hello world", () => {
-  test("hello world", () => {
-    const helloWorld = HelloWorld();
-    assert.equal(helloWorld, "Hello World", "failed to get hello world");
-  });
-});
