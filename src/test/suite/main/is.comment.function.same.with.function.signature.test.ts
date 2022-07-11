a639d0cdb058c080287df69b3aaac803d397c02d
jedi <amajidsinar@gmail.com>
2022-07-11 18:46:21 +0100

    test: implicitArgs is different

--- src/test/suite/main/is.comment.function.same.with.function.signature.test.ts
+++ src/test/suite/main/is.comment.function.same.with.function.signature.test.ts
@@ -46,47 +46,47 @@ suite("isCommentFunctionSameWithFunctionSignature", () => {
     assert.equal(true, isValid)
 });
 
-// test("implicitArgs is different", () => {
+test("implicitArgs is different", () => {
 
-//   const scopeLines = {
-//       attributeName: "view",
-//       functionName: "totalSupply",
-//       functionSignature: {
-//         implicitArgs: [
-//           { name: "pedersen_ptr", type: "HashBuiltin*" },
-//           { name: "range_check_ptr", type: "" },
-//         ],
-//         explicitArgs: null,
-//         returns: [{ name: "totalSupply", type: "Uint256" }],
-//       },
-//       functionComment: {
-//         desc: [
-//           {
-//             name: "",
-//             type: "",
-//             desc: "Returns the total supply of the token",
-//           },
-//         ],
-//         implicitArgs: [
-//           { name: "syscall_ptr", type: "felt*" , desc: ""},
-//           { name: "pedersen_ptr", type: "HashBuiltin*", desc: "" },
-//           { name: "range_check_ptr", type: "", desc: "" },
-//         ],
-//         explicitArgs: null,
-//         returns: [
-//           {
-//             name: "totalSupply",
-//             type: "Uint256",
-//             desc: "total supply of the token",
-//           },
-//         ],
-//         raises: null,
-//       },
-//     }
+  const scopeLines = {
+      attributeName: "view",
+      functionName: "totalSupply",
+      functionSignature: {
+        implicitArgs: [
+          { name: "pedersen_ptr", type: "HashBuiltin*" },
+          { name: "range_check_ptr", type: "" },
+        ],
+        explicitArgs: null,
+        returns: [{ name: "totalSupply", type: "Uint256" }],
+      },
+      functionComment: {
+        desc: [
+          {
+            name: "",
+            type: "",
+            desc: "Returns the total supply of the token",
+          },
+        ],
+        implicitArgs: [
+          { name: "syscall_ptr", type: "felt*" , desc: ""},
+          { name: "pedersen_ptr", type: "HashBuiltin*", desc: "" },
+          { name: "range_check_ptr", type: "", desc: "" },
+        ],
+        explicitArgs: null,
+        returns: [
+          {
+            name: "totalSupply",
+            type: "Uint256",
+            desc: "total supply of the token",
+          },
+        ],
+        raises: null,
+      },
+    }
     
-//   const isValid = CairoParser.isCommentFunctionSameWithFunctionSignature(scopeLines);
-//   assert.equal(false, isValid)
-// });
+  const isValid = CairoParser.isCommentFunctionSameWithFunctionSignature(scopeLines);
+  assert.equal(false, isValid)
+});
 
 // test("explicitArgs is the same", () => {
 
