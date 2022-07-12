2b722900f4dfa84ec0503416552fda733aeb7b58
jedi <amajidsinar@gmail.com>
2022-07-12 12:24:34 +0100

    test(explicit-args): commentFunction has less element

--- src/test/suite/main/isValidFunctionComment/explicitArgs.test.ts
+++ src/test/suite/main/isValidFunctionComment/explicitArgs.test.ts
@@ -65,63 +65,63 @@ suite("isValidFunctionComment: explicit args", () => {
     assert.deepEqual({isValid: true, errorSource: null}, isValid);
   });
 
-  // test("commentFunction has less element", () => {
-  //   const scopeLines = {
-  //     attributeName: "constructor",
-  //     functionName: "constructor",
-  //     functionSignature: {
-  //       implicitArgs: [
-  //         { name: "syscall_ptr", type: "felt*" },
-  //         { name: "pedersen_ptr", type: "HashBuiltin*" },
-  //         { name: "range_check_ptr", type: "" },
-  //       ],
-  //       explicitArgs: [
-  //         { name: "name", type: "felt" },
-  //         { name: "symbol", type: "felt" },
-  //         { name: "decimals", type: "Uint256" },
-  //         { name: "initial_supply", type: "Uint256" },
-  //         { name: "recipient", type: "felt" },
-  //       ],
-  //       returns: null,
-  //     },
-  //     functionComment: {
-  //       desc: [{ name: "", type: "", desc: "Initialize the contract" }],
-  //       implicitArgs: [
-  //         { name: "syscall_ptr", type: "felt*", desc: "" },
-  //         { name: "pedersen_ptr", type: "HashBuiltin", desc: "" },
-  //         { name: "range_check_ptr", type: "", desc: "" },
-  //       ],
-  //       explicitArgs: [
-  //         { name: "name", type: "felt", desc: "name of the token" },
-  //         { name: "symbol", type: "felt", desc: "symbol of the token" },
-  //         {
-  //           name: "decimals",
-  //           type: "Uint256",
-  //           desc: "floating point of the token",
-  //         },
-  //         {
-  //           name: "recipient",
-  //           type: "felt",
-  //           desc: "the address of recipient of the initial supply",
-  //         },
-  //       ],
-  //       returns: [{ name: "", type: "", desc: "None" }],
-  //       raises: [
-  //         { name: "decimals", type: "", desc: "decimals exceed 2^8" },
-  //         {
-  //           name: "recipient",
-  //           type: "",
-  //           desc: "cannot mint to the zero address",
-  //         },
-  //         { name: "initial_supply", type: "", desc: "not valid Uint256" },
-  //         { name: "initial_supply", type: "", desc: "mint overflow" },
-  //       ],
-  //     },
-  //   };
+  test("commentFunction has less element", () => {
+    const scopeLines = {
+      attributeName: "constructor",
+      functionName: "constructor",
+      functionSignature: {
+        implicitArgs: [
+          { name: "syscall_ptr", type: "felt*" },
+          { name: "pedersen_ptr", type: "HashBuiltin*" },
+          { name: "range_check_ptr", type: "" },
+        ],
+        explicitArgs: [
+          { name: "name", type: "felt" },
+          { name: "symbol", type: "felt" },
+          { name: "decimals", type: "Uint256" },
+          { name: "initial_supply", type: "Uint256" },
+          { name: "recipient", type: "felt" },
+        ],
+        returns: null,
+      },
+      functionComment: {
+        desc: [{ name: "", type: "", desc: "Initialize the contract" }],
+        implicitArgs: [
+          { name: "syscall_ptr", type: "felt*", desc: "" },
+          { name: "pedersen_ptr", type: "HashBuiltin*", desc: "" },
+          { name: "range_check_ptr", type: "", desc: "" },
+        ],
+        explicitArgs: [
+          { name: "name", type: "felt", desc: "name of the token" },
+          { name: "symbol", type: "felt", desc: "symbol of the token" },
+          {
+            name: "decimals",
+            type: "Uint256",
+            desc: "floating point of the token",
+          },
+          {
+            name: "recipient",
+            type: "felt",
+            desc: "the address of recipient of the initial supply",
+          },
+        ],
+        returns: null,
+        raises: [
+          { name: "decimals", type: "", desc: "decimals exceed 2^8" },
+          {
+            name: "recipient",
+            type: "",
+            desc: "cannot mint to the zero address",
+          },
+          { name: "initial_supply", type: "", desc: "not valid Uint256" },
+          { name: "initial_supply", type: "", desc: "mint overflow" },
+        ],
+      },
+    };
 
-  //   const isValid = CairoParser.isValidFunctionComment(scopeLines);
-  //   assert.deepEqual({isValid: false, errorSource: "explicitArgs"}, isValid);
-  // });
+    const isValid = CairoParser.isValidFunctionComment(scopeLines);
+    assert.deepEqual({isValid: false, errorSource: "explicitArgs"}, isValid);
+  });
 
   // test("commentFunction has more element", () => {
   //   const scopeLines = {
