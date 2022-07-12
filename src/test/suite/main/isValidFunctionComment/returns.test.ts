165dcea6b3682ff4136476de9b939f3565bcaf58
jedi <amajidsinar@gmail.com>
2022-07-12 14:56:36 +0100

    test(returns): both are null

--- src/test/suite/main/isValidFunctionComment/returns.test.ts
+++ src/test/suite/main/isValidFunctionComment/returns.test.ts
@@ -315,37 +315,59 @@ suite("isValidFunctionComment: returns", () => {
 
   
 
-  // test("both are null", () => {
-  //   const scopeLines = {
-  //     attributeName: "view",
-  //     functionName: "totalSupply",
-  //     functionSignature: {
-  //       implicitArgs: null,
-  //       explicitArgs: null,
-  //       returns: [{ name: "totalSupply", type: "Uint256" }],
-  //     },
-  //     functionComment: {
-  //       desc: [
-  //         {
-  //           name: "",
-  //           type: "",
-  //           desc: "Returns the total supply of the token",
-  //         },
-  //       ],
-  //       implicitArgs: null,
-  //       explicitArgs: null,
-  //       returns: [
-  //         {
-  //           name: "totalSupply",
-  //           type: "Uint256",
-  //           desc: "total supply of the token",
-  //         },
-  //       ],
-  //       raises: null,
-  //     },
-  //   };
+  test("both are null", () => {
+    const scopeLines = {
+      attributeName: "external",
+      functionName: "transfer",
+      functionSignature: {
+        implicitArgs: [
+          { name: "syscall_ptr", type: "felt*" },
+          { name: "pedersen_ptr", type: "HashBuiltin*" },
+          { name: "range_check_ptr", type: "" },
+        ],
+        explicitArgs: [
+          { name: "recipient", type: "felt" },
+          { name: "amount", type: "Uint256" },
+        ],
+        returns: null,
+      },
+      functionComment: {
+        desc: [{ name: "", type: "", desc: "Perform transfer to recipient" }],
+        implicitArgs: [
+          { name: "syscall_ptr", type: "felt*", desc: "" },
+          { name: "pedersen_ptr", type: "HashBuiltin*", desc: "" },
+          { name: "range_check_ptr", type: "", desc: "" },
+        ],
+        explicitArgs: [
+          {
+            name: "recipient",
+            type: "felt",
+            desc: "the address of ERC20 recipient",
+          },
+          {
+            name: "amount",
+            type: "Uint256",
+            desc: "the amount of ERC20 transfer",
+          },
+        ],
+        returns: null,
+        raises: [
+          { name: "amount", type: "", desc: "amount is not a valid Uint256" },
+          {
+            name: "recipient",
+            type: "",
+            desc: "cannot transfer to the zero address",
+          },
+          {
+            name: "amount",
+            type: "",
+            desc: "transfer amount exceeds balance",
+          },
+        ],
+      },
+    };
 
-  //   const isValid = CairoParser.isValidFunctionComment(scopeLines);
-  //   assert.deepEqual({isValid: true, errorSource: null}, isValid);
-  // });
+    const isValid = CairoParser.isValidFunctionComment(scopeLines);
+    assert.deepEqual({isValid: true, errorSource: null}, isValid);
+  });
 });
