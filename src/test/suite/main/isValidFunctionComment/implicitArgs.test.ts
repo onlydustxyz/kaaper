5fd34229bc0b2ba7c9601de27cfbb1447d438b76
jedi <amajidsinar@gmail.com>
2022-07-12 12:22:30 +0100

    test(explicit-args): valid (both are not none)

--- src/test/suite/main/isValidFunctionComment/implicitArgs.test.ts
+++ src/test/suite/main/isValidFunctionComment/implicitArgs.test.ts
@@ -1,233 +1,233 @@
-import * as assert from "assert";
-import CairoParser from "../../../../lib/main";
+// import * as assert from "assert";
+// import CairoParser from "../../../../lib/main";
 
-suite("isValidFunctionComment: implicit args", () => {
-  test("valid (both are not none)", () => {
-    const scopeLines = {
-      attributeName: "view",
-      functionName: "totalSupply",
-      functionSignature: {
-        implicitArgs: [
-          { name: "syscall_ptr", type: "felt*" },
-          { name: "pedersen_ptr", type: "HashBuiltin*" },
-          { name: "range_check_ptr", type: "" },
-        ],
-        explicitArgs: null,
-        returns: [{ name: "totalSupply", type: "Uint256" }],
-      },
-      functionComment: {
-        desc: [
-          {
-            name: "",
-            type: "",
-            desc: "Returns the total supply of the token",
-          },
-        ],
-        implicitArgs: [
-          { name: "syscall_ptr", type: "felt*", desc: "" },
-          { name: "pedersen_ptr", type: "HashBuiltin*", desc: "" },
-          { name: "range_check_ptr", type: "", desc: "" },
-        ],
-        explicitArgs: null,
-        returns: [
-          {
-            name: "totalSupply",
-            type: "Uint256",
-            desc: "total supply of the token",
-          },
-        ],
-        raises: null,
-      },
-    };
+// suite("isValidFunctionComment: implicit args", () => {
+//   test("valid (both are not none)", () => {
+//     const scopeLines = {
+//       attributeName: "view",
+//       functionName: "totalSupply",
+//       functionSignature: {
+//         implicitArgs: [
+//           { name: "syscall_ptr", type: "felt*" },
+//           { name: "pedersen_ptr", type: "HashBuiltin*" },
+//           { name: "range_check_ptr", type: "" },
+//         ],
+//         explicitArgs: null,
+//         returns: [{ name: "totalSupply", type: "Uint256" }],
+//       },
+//       functionComment: {
+//         desc: [
+//           {
+//             name: "",
+//             type: "",
+//             desc: "Returns the total supply of the token",
+//           },
+//         ],
+//         implicitArgs: [
+//           { name: "syscall_ptr", type: "felt*", desc: "" },
+//           { name: "pedersen_ptr", type: "HashBuiltin*", desc: "" },
+//           { name: "range_check_ptr", type: "", desc: "" },
+//         ],
+//         explicitArgs: null,
+//         returns: [
+//           {
+//             name: "totalSupply",
+//             type: "Uint256",
+//             desc: "total supply of the token",
+//           },
+//         ],
+//         raises: null,
+//       },
+//     };
 
-    const isValid = CairoParser.isValidFunctionComment(scopeLines);
-    assert.deepEqual({isValid: true, errorSource:null}, isValid);
-  });
+//     const isValid = CairoParser.isValidFunctionComment(scopeLines);
+//     assert.deepEqual({isValid: true, errorSource:null}, isValid);
+//   });
 
-  test("commentFunction has less element", () => {
-    const scopeLines = {
-      attributeName: "view",
-      functionName: "totalSupply",
-      functionSignature: {
-        implicitArgs: [
-          { name: "syscall_ptr", type: "felt*" },
-          { name: "pedersen_ptr", type: "HashBuiltin*" },
-          { name: "range_check_ptr", type: "" },
-        ],
-        explicitArgs: null,
-        returns: [{ name: "totalSupply", type: "Uint256" }],
-      },
-      functionComment: {
-        desc: [
-          {
-            name: "",
-            type: "",
-            desc: "Returns the total supply of the token",
-          },
-        ],
-        implicitArgs: [
-          { name: "syscall_ptr", type: "felt*", desc: "" },
-          { name: "range_check_ptr", type: "", desc: "" },
-        ],
-        explicitArgs: null,
-        returns: [
-          {
-            name: "totalSupply",
-            type: "Uint256",
-            desc: "total supply of the token",
-          },
-        ],
-        raises: null,
-      },
-    };
+//   test("commentFunction has less element", () => {
+//     const scopeLines = {
+//       attributeName: "view",
+//       functionName: "totalSupply",
+//       functionSignature: {
+//         implicitArgs: [
+//           { name: "syscall_ptr", type: "felt*" },
+//           { name: "pedersen_ptr", type: "HashBuiltin*" },
+//           { name: "range_check_ptr", type: "" },
+//         ],
+//         explicitArgs: null,
+//         returns: [{ name: "totalSupply", type: "Uint256" }],
+//       },
+//       functionComment: {
+//         desc: [
+//           {
+//             name: "",
+//             type: "",
+//             desc: "Returns the total supply of the token",
+//           },
+//         ],
+//         implicitArgs: [
+//           { name: "syscall_ptr", type: "felt*", desc: "" },
+//           { name: "range_check_ptr", type: "", desc: "" },
+//         ],
+//         explicitArgs: null,
+//         returns: [
+//           {
+//             name: "totalSupply",
+//             type: "Uint256",
+//             desc: "total supply of the token",
+//           },
+//         ],
+//         raises: null,
+//       },
+//     };
 
-    const isValid = CairoParser.isValidFunctionComment(scopeLines);
-    assert.deepEqual({isValid: false, errorSource:"implicitArgs"}, isValid);
-  });
+//     const isValid = CairoParser.isValidFunctionComment(scopeLines);
+//     assert.deepEqual({isValid: false, errorSource:"implicitArgs"}, isValid);
+//   });
 
-  test("commentFunction has more element", () => {
-    const scopeLines = {
-      attributeName: "view",
-      functionName: "totalSupply",
-      functionSignature: {
-        implicitArgs: [
-          { name: "syscall_ptr", type: "felt*" },
-          { name: "range_check_ptr", type: "" },
-        ],
-        explicitArgs: null,
-        returns: [{ name: "totalSupply", type: "Uint256" }],
-      },
-      functionComment: {
-        desc: [
-          {
-            name: "",
-            type: "",
-            desc: "Returns the total supply of the token",
-          },
-        ],
-        implicitArgs: [
-          { name: "syscall_ptr", type: "felt*", desc: "" },
-          { name: "pedersen_ptr", type: "HashBuiltin*", desc: "" },
-          { name: "range_check_ptr", type: "", desc: "" },
-        ],
-        explicitArgs: null,
-        returns: [
-          {
-            name: "totalSupply",
-            type: "Uint256",
-            desc: "total supply of the token",
-          },
-        ],
-        raises: null,
-      },
-    };
+//   test("commentFunction has more element", () => {
+//     const scopeLines = {
+//       attributeName: "view",
+//       functionName: "totalSupply",
+//       functionSignature: {
+//         implicitArgs: [
+//           { name: "syscall_ptr", type: "felt*" },
+//           { name: "range_check_ptr", type: "" },
+//         ],
+//         explicitArgs: null,
+//         returns: [{ name: "totalSupply", type: "Uint256" }],
+//       },
+//       functionComment: {
+//         desc: [
+//           {
+//             name: "",
+//             type: "",
+//             desc: "Returns the total supply of the token",
+//           },
+//         ],
+//         implicitArgs: [
+//           { name: "syscall_ptr", type: "felt*", desc: "" },
+//           { name: "pedersen_ptr", type: "HashBuiltin*", desc: "" },
+//           { name: "range_check_ptr", type: "", desc: "" },
+//         ],
+//         explicitArgs: null,
+//         returns: [
+//           {
+//             name: "totalSupply",
+//             type: "Uint256",
+//             desc: "total supply of the token",
+//           },
+//         ],
+//         raises: null,
+//       },
+//     };
 
-    const isValid = CairoParser.isValidFunctionComment(scopeLines);
-    assert.deepEqual({isValid: false, errorSource:"implicitArgs"}, isValid);
-  });
+//     const isValid = CairoParser.isValidFunctionComment(scopeLines);
+//     assert.deepEqual({isValid: false, errorSource:"implicitArgs"}, isValid);
+//   });
 
-  test("functionSignature is null", () => {
-    const scopeLines = {
-      attributeName: "view",
-      functionName: "totalSupply",
-      functionSignature: {
-        implicitArgs: null,
-        explicitArgs: null,
-        returns: [{ name: "totalSupply", type: "Uint256" }],
-      },
-      functionComment: {
-        desc: [
-          {
-            name: "",
-            type: "",
-            desc: "Returns the total supply of the token",
-          },
-        ],
-        implicitArgs: [
-          { name: "syscall_ptr", type: "felt*", desc: "" },
-          { name: "range_check_ptr", type: "", desc: "" },
-        ],
-        explicitArgs: null,
-        returns: [
-          {
-            name: "totalSupply",
-            type: "Uint256",
-            desc: "total supply of the token",
-          },
-        ],
-        raises: null,
-      },
-    };
+//   test("functionSignature is null", () => {
+//     const scopeLines = {
+//       attributeName: "view",
+//       functionName: "totalSupply",
+//       functionSignature: {
+//         implicitArgs: null,
+//         explicitArgs: null,
+//         returns: [{ name: "totalSupply", type: "Uint256" }],
+//       },
+//       functionComment: {
+//         desc: [
+//           {
+//             name: "",
+//             type: "",
+//             desc: "Returns the total supply of the token",
+//           },
+//         ],
+//         implicitArgs: [
+//           { name: "syscall_ptr", type: "felt*", desc: "" },
+//           { name: "range_check_ptr", type: "", desc: "" },
+//         ],
+//         explicitArgs: null,
+//         returns: [
+//           {
+//             name: "totalSupply",
+//             type: "Uint256",
+//             desc: "total supply of the token",
+//           },
+//         ],
+//         raises: null,
+//       },
+//     };
 
-    const isValid = CairoParser.isValidFunctionComment(scopeLines);
-    assert.deepEqual({isValid: false, errorSource:"implicitArgs"}, isValid);
-  });
+//     const isValid = CairoParser.isValidFunctionComment(scopeLines);
+//     assert.deepEqual({isValid: false, errorSource:"implicitArgs"}, isValid);
+//   });
 
-  test("functionComment is null", () => {
-    const scopeLines = {
-      attributeName: "view",
-      functionName: "totalSupply",
-      functionSignature: {
-        implicitArgs: [{ name: "syscall_ptr", type: "felt*", desc: "" }],
-        explicitArgs: null,
-        returns: [{ name: "totalSupply", type: "Uint256" }],
-      },
-      functionComment: {
-        desc: [
-          {
-            name: "",
-            type: "",
-            desc: "Returns the total supply of the token",
-          },
-        ],
-        implicitArgs: null,
-        explicitArgs: null,
-        returns: [
-          {
-            name: "totalSupply",
-            type: "Uint256",
-            desc: "total supply of the token",
-          },
-        ],
-        raises: null,
-      },
-    };
+//   test("functionComment is null", () => {
+//     const scopeLines = {
+//       attributeName: "view",
+//       functionName: "totalSupply",
+//       functionSignature: {
+//         implicitArgs: [{ name: "syscall_ptr", type: "felt*", desc: "" }],
+//         explicitArgs: null,
+//         returns: [{ name: "totalSupply", type: "Uint256" }],
+//       },
+//       functionComment: {
+//         desc: [
+//           {
+//             name: "",
+//             type: "",
+//             desc: "Returns the total supply of the token",
+//           },
+//         ],
+//         implicitArgs: null,
+//         explicitArgs: null,
+//         returns: [
+//           {
+//             name: "totalSupply",
+//             type: "Uint256",
+//             desc: "total supply of the token",
+//           },
+//         ],
+//         raises: null,
+//       },
+//     };
 
-    const isValid = CairoParser.isValidFunctionComment(scopeLines);
-    assert.deepEqual({isValid: false, errorSource:"implicitArgs"}, isValid);
-  });
+//     const isValid = CairoParser.isValidFunctionComment(scopeLines);
+//     assert.deepEqual({isValid: false, errorSource:"implicitArgs"}, isValid);
+//   });
 
-  test("both are null", () => {
-    const scopeLines = {
-      attributeName: "view",
-      functionName: "totalSupply",
-      functionSignature: {
-        implicitArgs: null,
-        explicitArgs: null,
-        returns: [{ name: "totalSupply", type: "Uint256" }],
-      },
-      functionComment: {
-        desc: [
-          {
-            name: "",
-            type: "",
-            desc: "Returns the total supply of the token",
-          },
-        ],
-        implicitArgs: null,
-        explicitArgs: null,
-        returns: [
-          {
-            name: "totalSupply",
-            type: "Uint256",
-            desc: "total supply of the token",
-          },
-        ],
-        raises: null,
-      },
-    };
+//   test("both are null", () => {
+//     const scopeLines = {
+//       attributeName: "view",
+//       functionName: "totalSupply",
+//       functionSignature: {
+//         implicitArgs: null,
+//         explicitArgs: null,
+//         returns: [{ name: "totalSupply", type: "Uint256" }],
+//       },
+//       functionComment: {
+//         desc: [
+//           {
+//             name: "",
+//             type: "",
+//             desc: "Returns the total supply of the token",
+//           },
+//         ],
+//         implicitArgs: null,
+//         explicitArgs: null,
+//         returns: [
+//           {
+//             name: "totalSupply",
+//             type: "Uint256",
+//             desc: "total supply of the token",
+//           },
+//         ],
+//         raises: null,
+//       },
+//     };
 
-    const isValid = CairoParser.isValidFunctionComment(scopeLines);
-    assert.deepEqual({isValid: true, errorSource:null}, isValid);
-  });
-});
+//     const isValid = CairoParser.isValidFunctionComment(scopeLines);
+//     assert.deepEqual({isValid: true, errorSource:null}, isValid);
+//   });
+// });
