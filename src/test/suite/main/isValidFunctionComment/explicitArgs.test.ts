ca1bf162ee32026ed1fa0df4dfcf71a210f4f613
jedi <amajidsinar@gmail.com>
2022-07-13 16:33:07 +0100

    fix

--- src/test/suite/main/isValidFunctionComment/explicitArgs.test.ts
+++ src/test/suite/main/isValidFunctionComment/explicitArgs.test.ts
@@ -267,69 +267,6 @@ suite("isValidFunctionComment: explicit args", () => {
           { name: "pedersen_ptr", type: "HashBuiltin*", desc: "" },
           { name: "range_check_ptr", type: "", desc: "" },
         ],
-        explicitArgs: [
-          { name: "name", type: "felt", desc: "name of the token" },
-          { name: "symbol", type: "felt", desc: "symbol of the token" },
-          {
-            name: "decimals",
-            type: "Uint256",
-            desc: "floating point of the token",
-          },
-          {
-            name: "initial_supply",
-            type: "Uint256",
-            desc: "amount of initial supply of the token",
-          },
-          {
-            name: "recipient",
-            type: "felt",
-            desc: "the address of recipient of the initial supply",
-          },
-        ],
-        returns: null,
-        raises: [
-          { name: "decimals", type: "", desc: "decimals exceed 2^8" },
-          {
-            name: "recipient",
-            type: "",
-            desc: "cannot mint to the zero address",
-          },
-          { name: "initial_supply", type: "", desc: "not valid Uint256" },
-          { name: "initial_supply", type: "", desc: "mint overflow" },
-        ],
-      },
-    };
-
-    const isValid = CairoParser.isValidFunctionComment(scopeLines);
-    assert.deepEqual({ isValid: false, errorSource: "explicitArgs" }, isValid);
-  });
-
-  test("functionComment is null", () => {
-    const scopeLines = {
-      attributeName: "constructor",
-      functionName: "constructor",
-      functionSignature: {
-        implicitArgs: [
-          { name: "syscall_ptr", type: "felt*" },
-          { name: "pedersen_ptr", type: "HashBuiltin*" },
-          { name: "range_check_ptr", type: "" },
-        ],
-        explicitArgs: [
-          { name: "name", type: "felt" },
-          { name: "symbol", type: "felt" },
-          { name: "decimals", type: "Uint256" },
-          { name: "initial_supply", type: "Uint256" },
-          { name: "recipient", type: "felt" },
-        ],
-        returns: null,
-      },
-      functionComment: {
-        desc: [{ name: "", type: "", desc: "Initialize the contract" }],
-        implicitArgs: [
-          { name: "syscall_ptr", type: "felt*", desc: "" },
-          { name: "pedersen_ptr", type: "HashBuiltin*", desc: "" },
-          { name: "range_check_ptr", type: "", desc: "" },
-        ],
         explicitArgs: null,
         returns: null,
         raises: [
