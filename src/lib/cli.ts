906121466f204f26ab815aecf0298b16d7a047f7
jedi <amajidsinar@gmail.com>
2022-07-13 23:44:12 +0100

    feat(checkContractsCommentCompliance): CommentComplicance[] as return

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -26,8 +26,9 @@ export default class CLI {
     }
   }
 
-  checkContractsCommentCompliance(): CommentComplicance {
+  checkContractsCommentCompliance(): CommentComplicance[] {
     const contractPaths = fs.readdirSync(this.contractRootDir);
+    var contractsCommentCompliance: CommentComplicance[] = [];
     for (const contractFile of contractPaths) {
       const filePath = path.join(this.contractRootDir, contractFile);
       const parsingResults = CairoParser.getFileParsingResult(filePath);
@@ -35,15 +36,18 @@ export default class CLI {
         for (const parsingResult of parsingResults) {
           const isValid = CairoParser.isValidFunctionComment(parsingResult);
           if (isValid.isValid === false) {
-            return {
-              isCompliant: false,
-              filePath: filePath,
-              errorSource: isValid.errorSource,
-            };
+            console.log(
+              `comment on ${filePath}: ${parsingResult.functionName} - ${isValid.errorSource} is not valid`
+            );
           }
+          contractsCommentCompliance.push({
+            isCompliant: isValid.isValid,
+            filePath: filePath,
+            errorSource: isValid.errorSource,
+          });
         }
       }
     }
-    return { isCompliant: true, filePath: null, errorSource: null };
+    return contractsCommentCompliance;
   }
 }
