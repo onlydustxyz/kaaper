348cd580ad30cba4a7dda7039bdb07f095272262
jedi <amajidsinar@gmail.com>
2022-07-14 14:50:15 +0100

    feat(index.ts): add cli

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ const figlet = require("figlet");
 const path = require("path");
 const program = require("commander");
 
-// import CairoParser from './lib/CairoParser";
 import CLI from "./lib/cli";
 
 clear();
@@ -15,30 +14,21 @@ console.log(chalk.red(figlet.textSync("kaaper", { horizontalLayout: "full" })));
 program
   .name("kaaper")
   .description("CLI to help cairo developers")
-  .version("0.0.1");
+  .version("0.0.2");
 
 program
   .command("generate")
   .description("Generate contract docs")
-  .argument("<string>", "string to split")
-  .option("-r, --root <string>", "root directory of contracts")
-  .action((str: any, options: any) => {
-    const cli = new CLI(options.root);
-    cli.generateContractsDocs(options.root, false);
-    console.log("root:", options.root);
-    // const limit = options.first ? 1 : undefined;
-    // console.log(str.split(options.separator, limit));
+  .argument("<rootdir>", "root directory of contracts")
+  .argument(["outdir"], "output directory")
+  .action((rootdir: any, outdir: any) => {
+    const cli = new CLI(rootdir);
+    cli.generateContractsDocs(outdir, false);
+    console.log("documents generated at:", outdir);
   });
 
 program.parse(process.argv);
 
-//   console.log('you ordered a pizza with:');
-//   if (program.peppers) console.log('  - peppers');
-//   if (program.pineapple) console.log('  - pineapple');
-//   if (program.bbq) console.log('  - bbq');
-//   const cheese: string = true === program.cheese ? 'marble' : program.cheese || 'no';
-//   console.log('  - %s cheese', cheese);
-
-//   if (!process.argv.slice(2).length) {
-//     program.outputHelp();
-//   }
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
