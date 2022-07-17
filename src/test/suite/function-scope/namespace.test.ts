b6348035a2e2a431ed38f06ffc940299be3a8dde
jedi <amajidsinar@gmail.com>
2022-07-17 18:12:04 +0100

    test(namespace-scope): #1

--- src/test/suite/function-scope/namespace.test.ts
+++ src/test/suite/function-scope/namespace.test.ts
@@ -10,7 +10,7 @@ import FunctionCommentReturnsParser from "../../../lib/parser/function-comment/r
 import FunctionCommentRaisesParser from "../../../lib/parser/function-comment/raises";
 
 suite("integration-test: namespace", () => {
-  test("namespace ERC20", () => {
+  test("0 ", () => {
     const pathFile = path.resolve(
       __dirname,
       "../../../../testContracts/ERC20Namespace/library.cairo"
@@ -77,6 +77,115 @@ suite("integration-test: namespace", () => {
             },
           ],
           returns: null,
+          raises: null,
+        },
+      },
+    ];
+
+    const parsingOutput = [
+      {
+        attributeName: functionSignatureParser.getAttributeName(
+          functionScopeLines![line]
+        ),
+        functionName: functionSignatureParser.getFunctionName(
+          functionScopeLines![line]
+        ),
+        functionSignature: {
+          implicitArgs: functionSignatureParser.getImplicitArgs(
+            functionScopeLines![line]
+          ),
+          explicitArgs: functionSignatureParser.getExplicitArgs(
+            functionScopeLines![line]
+          ),
+          returns: functionSignatureParser.getReturns(
+            functionScopeLines![line]
+          ),
+        },
+        functionComment: {
+          desc: functionCommentDescParser.parseCommentLines(commentLines!),
+          implicitArgs: functionCommentImplicitArgsParser.parseCommentLines(
+            commentLines!
+          ),
+          explicitArgs: functionCommentExplicitArgsParser.parseCommentLines(
+            commentLines!
+          ),
+          returns: functionCommentReturnsParser.parseCommentLines(
+            commentLines!
+          ),
+          raises: functionCommentRaisesParser.parseCommentLines(commentLines!),
+        },
+      },
+    ];
+
+    assert.deepEqual(parsingTarget, parsingOutput, "failed to parse");
+  });
+
+  test("1 ", () => {
+    const pathFile = path.resolve(
+      __dirname,
+      "../../../../testContracts/ERC20Namespace/library.cairo"
+    );
+    const text = fs.readFileSync(pathFile, "utf8");
+    // parse whole scope
+    const functionScopeLines = CairoParser.parseNamespaceScopes(text);
+
+    // Function signature parsing
+    const functionSignatureParser = new FunctionSignatureRegexParser();
+
+    // Comment parsing
+    // parse comment lines
+    const line = 1;
+    const commentLines = CairoParser.parseCommentLines(
+      functionScopeLines![line]
+    );
+
+    const functionCommentDescParser = new FunctionCommentDescParser();
+    const functionCommentImplicitArgsParser =
+      new FunctionCommentImplicitArgsParser();
+    const functionCommentExplicitArgsParser =
+      new FunctionCommentExplicitArgsParser();
+    const functionCommentReturnsParser = new FunctionCommentReturnsParser();
+    const functionCommentRaisesParser = new FunctionCommentRaisesParser();
+
+    const parsingTarget = [
+      {
+        attributeName: "namespace ERC20",
+        functionName: "name",
+        functionSignature: {
+          implicitArgs: [
+            { name: "syscall_ptr", type: "felt*" },
+            { name: "pedersen_ptr", type: "HashBuiltin*" },
+            { name: "range_check_ptr", type: "" },
+          ],
+          explicitArgs: null,
+          returns: [
+            {
+              name: "name",
+              type: "felt",
+            },
+          ],
+        },
+        functionComment: {
+          desc: [
+            {
+              name: "",
+              type: "",
+              desc: "Returns the name of the token",
+            },
+          ],
+          implicitArgs: [
+            { name: "syscall_ptr", type: "felt*", desc: "" },
+            { name: "pedersen_ptr", type: "HashBuiltin*", desc: "" },
+            { name: "range_check_ptr", type: "", desc: "" },
+          ],
+          explicitArgs: null,
+          returns: [
+            {
+              name: "name",
+              type: "felt",
+              desc: "The name of the token",
+            },
+          ],
           raises: null,
         },
       },
