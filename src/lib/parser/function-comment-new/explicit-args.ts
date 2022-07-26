e4d0a2bb2b179bf759aaa0462c02cc59bd6477ef
jedi <amajidsinar@gmail.com>
2022-07-26 21:56:14 +0100

    feat(parseCommentLine): fix logic

--- src/lib/parser/function-comment-new/explicit-args.ts
+++ src/lib/parser/function-comment-new/explicit-args.ts
@@ -9,24 +9,26 @@ export default class FunctionCommentExplicitArgsParser extends BaseCommentParser
 
   parseCommentLine(line: string): FunctionCommentNew | null {
     if (this.runningScope === true && this.startLine !== line) {
-      const regexp = new RegExp(`${line}`, "gm");
+      const regexp = /(\w+)(\((\w+)\)):(.*)/gm;
       const functionComments = [...this.functionComment.matchAll(regexp)];
-
-      if (functionComments.length > 0) {
-        const functionComment = line.match(/(\w+)(\((\w+)\)):(.*)/);
-        const start = functionComments[0].index!;
-        if (functionComment) {
-          return {
-            name: functionComment[1],
-            type: functionComment[3],
+      for (var functionComment of functionComments) {
+        // without # or anything else, just pure content
+        // e.g name(felt): The name of the token instead of 
+        // # name(felt): The name of the token
+        const commentContentOnly = line.match(regexp);
+        if (functionComment[0] === commentContentOnly![0]) {
+          const start = functionComment.index!;
+          const matchInterface = {
+            name: functionComment[1].trim(),
+            type: functionComment[3].trim(),
             desc: functionComment[4].trim(),
             charIndex: {
               start: start,
               end: start + line.length,
             },
           };
+          return matchInterface;
         }
-        return null;
       }
       return null;
     }
