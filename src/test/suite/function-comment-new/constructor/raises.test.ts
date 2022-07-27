067e1018b360660eb8a30d04f3b6ca86b3f74613
jedi <amajidsinar@gmail.com>
2022-07-27 18:43:43 +0100

    test(FunctionCommentRaisesParser): #17

--- src/test/suite/function-comment-new/constructor/raises.test.ts
+++ src/test/suite/function-comment-new/constructor/raises.test.ts
@@ -121,45 +121,145 @@ suite("function-comment: constructor: raises", () => {
     assert.equal(functionCommentReference, wholeFileReference);
   });
 
-  // test("parse line 16", () => {
-  //   const pathFile = path.resolve(
-  //     __dirname,
-  //     "../../../../../testContracts/ERC20Compliant/ERC20.cairo"
-  //   );
-  //   const text = fs.readFileSync(pathFile, "utf8");
-  //   const functionScopes = CairoParser.parseFunctionScopeWithMatchAll(text, "constructor");
-  //   const commentText = CairoParser.parseCommentLinesWithMatchAll(functionScopes![0])!.text;
-  //   const raisesParser = new FunctionCommentRaisesParser();
-  //   raisesParser.setStartScope(commentText![14]);
+  test("parse line 16", () => {
+    const pathFile = path.resolve(
+      __dirname,
+      "../../../../../testContracts/ERC20Compliant/ERC20.cairo"
+    );
+    const text = fs.readFileSync(pathFile, "utf8");
+    const functionScopes = CairoParser.parseFunctionScopeWithMatchAll(
+      text,
+      "constructor"
+    );
+    const functionScope = functionScopes![0];
+    const functionCommentScope =
+      CairoParser.parseCommentLinesWithMatchAll(functionScope)!;
+    const functionCommentText = functionCommentScope!.text.join("");
+    const raisesParser = new FunctionCommentRaisesParser(functionCommentText);
+    raisesParser.setStartScope(functionCommentScope!.text[14]);
 
-  //   const line = 16;
-  //   assert.equal(
-  //     "#   recipient: cannot mint to the zero address",
-  //     commentText![line].trim(),
-  //     `check line ${line}`
-  //   );
-  //   assert.notEqual(commentText![line], raisesParser.startLine);
-  //   const isEndScope = raisesParser.isEndScope(commentText![line]);
-  //   assert.equal(false, isEndScope, `failed to get end scope line ${line}`);
+    const line = 16;
+    const functionCommentLine = functionCommentScope!.text[line];
+    assert.equal(
+      "#   recipient: cannot mint to the zero address",
+      functionCommentLine.trim(),
+      `check line ${line}`
+    );
+    assert.notEqual(functionCommentLine, raisesParser.startLine);
+    const isEndScope = raisesParser.isEndScope(functionCommentLine);
+    assert.equal(false, isEndScope, `failed to get end scope line ${line}`);
 
-  //   assert.equal(
-  //     true,
-  //     raisesParser.runningScope,
-  //     `failed to get running scope line ${line}`
-  //   );
-  //   const resultLineParsing = raisesParser.parseCommentLine(commentText![line]);
+    assert.equal(
+      true,
+      raisesParser.runningScope,
+      `failed to get running scope line ${line}`
+    );
+    const resultLineParsing =
+      raisesParser.parseCommentLine(functionCommentLine);
 
-  //   const targetLineParsing = {
-  //     name: "recipient",
-  //     type: "",
-  //     desc: "cannot mint to the zero address",
-  //   };
-  //   assert.deepEqual(
-  //     targetLineParsing,
-  //     resultLineParsing,
-  //     `failed to get resultLineParsing line ${line}`
-  //   );
-  // });
+    const targetLineParsing = {
+      name: "recipient",
+      type: "",
+      desc: "cannot mint to the zero address",
+      charIndex: {
+        start: 539,
+        end: 581,
+      },
+    };
+    assert.deepEqual(
+      targetLineParsing,
+      resultLineParsing,
+      `failed to get resultLineParsing line ${line}`
+    );
+
+    var functionCommentReference = "";
+    const explicitArgsCommentStart = resultLineParsing!.charIndex.start;
+    const explicitArgsCommentEnd = resultLineParsing!.charIndex.end;
+    for (var i = explicitArgsCommentStart; i < explicitArgsCommentEnd; i++) {
+      functionCommentReference += functionCommentText[i];
+    }
+
+    var wholeFileReference = "";
+    const functionCommentStart = functionCommentScope!.start;
+    for (
+      var i = functionCommentStart + explicitArgsCommentStart;
+      i < functionCommentStart + explicitArgsCommentEnd;
+      i++
+    ) {
+      wholeFileReference += text[i];
+    }
+    assert.equal(functionCommentReference, wholeFileReference);
+  });
+
+  test("parse line 17", () => {
+    const pathFile = path.resolve(
+      __dirname,
+      "../../../../../testContracts/ERC20Compliant/ERC20.cairo"
+    );
+    const text = fs.readFileSync(pathFile, "utf8");
+    const functionScopes = CairoParser.parseFunctionScopeWithMatchAll(
+      text,
+      "constructor"
+    );
+    const functionScope = functionScopes![0];
+    const functionCommentScope =
+      CairoParser.parseCommentLinesWithMatchAll(functionScope)!;
+    const functionCommentText = functionCommentScope!.text.join("");
+    const raisesParser = new FunctionCommentRaisesParser(functionCommentText);
+    raisesParser.setStartScope(functionCommentScope!.text[14]);
+
+    const line = 17;
+    const functionCommentLine = functionCommentScope!.text[line];
+    assert.equal(
+      "#   initial_supply: not valid Uint256",
+      functionCommentLine.trim(),
+      `check line ${line}`
+    );
+    assert.notEqual(functionCommentLine, raisesParser.startLine);
+    const isEndScope = raisesParser.isEndScope(functionCommentLine);
+    assert.equal(false, isEndScope, `failed to get end scope line ${line}`);
+
+    assert.equal(
+      true,
+      raisesParser.runningScope,
+      `failed to get running scope line ${line}`
+    );
+    const resultLineParsing =
+      raisesParser.parseCommentLine(functionCommentLine);
+
+    const targetLineParsing = {
+      name: "initial_supply",
+      type: "",
+      desc: "not valid Uint256",
+      charIndex: {
+        start: 590,
+        end: 623,
+      },
+    };
+    assert.deepEqual(
+      targetLineParsing,
+      resultLineParsing,
+      `failed to get resultLineParsing line ${line}`
+    );
+
+    var functionCommentReference = "";
+    const explicitArgsCommentStart = resultLineParsing!.charIndex.start;
+    const explicitArgsCommentEnd = resultLineParsing!.charIndex.end;
+    for (var i = explicitArgsCommentStart; i < explicitArgsCommentEnd; i++) {
+      functionCommentReference += functionCommentText[i];
+    }
+
+    var wholeFileReference = "";
+    const functionCommentStart = functionCommentScope!.start;
+    for (
+      var i = functionCommentStart + explicitArgsCommentStart;
+      i < functionCommentStart + explicitArgsCommentEnd;
+      i++
+    ) {
+      wholeFileReference += text[i];
+    }
+    assert.equal(functionCommentReference, wholeFileReference);
+  });
 
   // test("parse line 17", () => {
   //   const pathFile = path.resolve(
