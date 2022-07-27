4f2a5c4a25fc6e98ff7b009b78a952a14519b1d4
jedi <amajidsinar@gmail.com>
2022-07-27 18:30:29 +0100

    feat(FunctionCommentRaisesParser): fix desc and type

--- src/lib/parser/function-comment-new/raises.ts
+++ src/lib/parser/function-comment-new/raises.ts
@@ -7,35 +7,6 @@ export default class FunctionCommentRaisesParser extends BaseCommentParser {
     this.name = "Raises";
   }
 
-  www(line: string): FunctionCommentNew | null {
-    if (this.runningScope === true && this.startLine !== line) {
-      const regexp = /((\w+)(\(?([\w\*]+)\))?)$/gm;
-      const functionComments = [...this.functionComment.matchAll(regexp)];
-      for (var functionComment of functionComments) {
-        // without # or anything else, just pure content
-        // e.g `pedersen_ptr(HashBuiltin*)` instead of
-        // #     pedersen_ptr(HashBuiltin*)
-        const commentLine = [...line.matchAll(regexp)];
-        if (functionComment[0] === commentLine![0][0]) {
-          const start = functionComment.index!;
-          const type = functionComment[4] ? functionComment[4].trim() : "";
-          const matchInterface = {
-            name: functionComment[2].trim(),
-            type: type,
-            desc: "",
-            charIndex: {
-              start: start,
-              end: start + functionComment[0].length,
-            },
-          };
-          return matchInterface;
-        }
-      }
-      return null;
-    }
-    return null;
-  }
-
   parseCommentLine(line: string): FunctionCommentNew | null {
     if (this.runningScope === true && this.startLine !== line) {
       if (line.includes("None")) {
@@ -44,17 +15,13 @@ export default class FunctionCommentRaisesParser extends BaseCommentParser {
       const regexp = /(\w+):\s*([\w\s\^]+)$/gm;
       const functionComments = [...this.functionComment.matchAll(regexp)];
       for (var functionComment of functionComments) {
-        // without # or anything else, just pure content
-        // e.g `pedersen_ptr(HashBuiltin*)` instead of
-        // #     pedersen_ptr(HashBuiltin*)
         const commentLine = [...line.matchAll(regexp)];
         if (functionComment[0] === commentLine![0][0]) {
           const start = functionComment.index!;
-          const type = functionComment[2] ? functionComment[2].trim() : "";
           const matchInterface = {
             name: functionComment[1].trim(),
-            type: type,
-            desc: "",
+            type: "",
+            desc: functionComment[2].trim(),
             charIndex: {
               start: start,
               end: start + functionComment[0].length,
--- src/test/suite/function-comment-new/constructor/raises.test.ts
+++ src/test/suite/function-comment-new/constructor/raises.test.ts
@@ -51,45 +51,75 @@ suite("function-comment: constructor: raises", () => {
     );
   });
 
-  // test("parse line 15", () => {
-  //   const pathFile = path.resolve(
-  //     __dirname,
-  //     "../../../../../testContracts/ERC20Compliant/ERC20.cairo"
-  //   );
-  //   const text = fs.readFileSync(pathFile, "utf8");
-  //   const functionCommentScope = CairoParser.parseFunctionScope(text, "constructor");
-  //   const commentText = CairoParser.parseCommentLines(functionCommentScope![0])!.text;
-  //   const raisesParser = new FunctionCommentRaisesParser();
-  //   raisesParser.setStartScope(commentText![14]);
+  test("parse line 15", () => {
+    const pathFile = path.resolve(
+      __dirname,
+      "../../../../../testContracts/ERC20Compliant/ERC20.cairo"
+    );
+    const text = fs.readFileSync(pathFile, "utf8");
+    const functionScopes = CairoParser.parseFunctionScopeWithMatchAll(
+      text,
+      "constructor"
+    );
+    const functionScope = functionScopes![0];
+    const functionCommentScope =
+      CairoParser.parseCommentLinesWithMatchAll(functionScope)!;
+    const functionCommentText = functionCommentScope!.text.join("");
+    const raisesParser = new FunctionCommentRaisesParser(functionCommentText);
+    raisesParser.setStartScope(functionCommentScope!.text[14]);
 
-  //   const line = 15;
-  //   assert.equal(
-  //     "#   decimals: decimals exceed 2^8",
-  //     commentText![line].trim(),
-  //     `check line ${line}`
-  //   );
-  //   assert.notEqual(commentText![line], raisesParser.startLine);
-  //   const isEndScope = raisesParser.isEndScope(commentText![line]);
-  //   assert.equal(false, isEndScope, `failed to get end scope line ${line}`);
+    const line = 15;
+    const functionCommentLine = functionCommentScope!.text[line];
+    assert.equal(
+      "#   decimals: decimals exceed 2^8",
+      functionCommentLine.trim(),
+      `check line ${line}`
+    );
+    assert.notEqual(functionCommentLine, raisesParser.startLine);
+    const isEndScope = raisesParser.isEndScope(functionCommentLine);
+    assert.equal(false, isEndScope, `failed to get end scope line ${line}`);
 
-  //   assert.equal(
-  //     true,
-  //     raisesParser.runningScope,
-  //     `failed to get running scope line ${line}`
-  //   );
-  //   const resultLineParsing = raisesParser.parseCommentLine(commentText![line]);
+    assert.equal(
+      true,
+      raisesParser.runningScope,
+      `failed to get running scope line ${line}`
+    );
+    const resultLineParsing =
+      raisesParser.parseCommentLine(functionCommentLine);
 
-  //   const targetLineParsing = {
-  //     name: "decimals",
-  //     type: "",
-  //     desc: "decimals exceed 2^8",
-  //   };
-  //   assert.deepEqual(
-  //     targetLineParsing,
-  //     resultLineParsing,
-  //     `failed to get resultLineParsing line ${line}`
-  //   );
-  // });
+    const targetLineParsing = {
+      name: "decimals",
+      type: "",
+      desc: "decimals exceed 2^8",
+      charIndex: {
+        start: 501,
+        end: 530,
+      },
+    };
+    assert.deepEqual(
+      targetLineParsing,
+      resultLineParsing,
+      `failed to get resultLineParsing line ${line}`
+    );
+
+    var functionCommentReference = "";
+    const explicitArgsCommentStart = resultLineParsing!.charIndex.start;
+    const explicitArgsCommentEnd = resultLineParsing!.charIndex.end;
+    for (var i = explicitArgsCommentStart; i < explicitArgsCommentEnd; i++) {
+      functionCommentReference += functionCommentText[i];
+    }
+
+    var wholeFileReference = "";
+    const functionCommentStart = functionCommentScope!.start;
+    for (
+      var i = functionCommentStart + explicitArgsCommentStart;
+      i < functionCommentStart + explicitArgsCommentEnd;
+      i++
+    ) {
+      wholeFileReference += text[i];
+    }
+    assert.equal(functionCommentReference, wholeFileReference);
+  });
 
   // test("parse line 16", () => {
   //   const pathFile = path.resolve(
@@ -97,8 +127,8 @@ suite("function-comment: constructor: raises", () => {
   //     "../../../../../testContracts/ERC20Compliant/ERC20.cairo"
   //   );
   //   const text = fs.readFileSync(pathFile, "utf8");
-  //   const functionCommentScope = CairoParser.parseFunctionScope(text, "constructor");
-  //   const commentText = CairoParser.parseCommentLines(functionCommentScope![0])!.text;
+  //   const functionScopes = CairoParser.parseFunctionScopeWithMatchAll(text, "constructor");
+  //   const commentText = CairoParser.parseCommentLinesWithMatchAll(functionScopes![0])!.text;
   //   const raisesParser = new FunctionCommentRaisesParser();
   //   raisesParser.setStartScope(commentText![14]);
 
@@ -137,8 +167,8 @@ suite("function-comment: constructor: raises", () => {
   //     "../../../../../testContracts/ERC20Compliant/ERC20.cairo"
   //   );
   //   const text = fs.readFileSync(pathFile, "utf8");
-  //   const functionCommentScope = CairoParser.parseFunctionScope(text, "constructor");
-  //   const commentText = CairoParser.parseCommentLines(functionCommentScope![0])!.text;
+  //   const functionScopes = CairoParser.parseFunctionScopeWithMatchAll(text, "constructor");
+  //   const commentText = CairoParser.parseCommentLinesWithMatchAll(functionScopes![0])!.text;
   //   const raisesParser = new FunctionCommentRaisesParser();
   //   raisesParser.setStartScope(commentText![14]);
 
@@ -177,8 +207,8 @@ suite("function-comment: constructor: raises", () => {
   //     "../../../../../testContracts/ERC20Compliant/ERC20.cairo"
   //   );
   //   const text = fs.readFileSync(pathFile, "utf8");
-  //   const functionCommentScope = CairoParser.parseFunctionScope(text, "constructor");
-  //   const commentText = CairoParser.parseCommentLines(functionCommentScope![0])!.text;
+  //   const functionScopes = CairoParser.parseFunctionScopeWithMatchAll(text, "constructor");
+  //   const commentText = CairoParser.parseCommentLinesWithMatchAll(functionScopes![0])!.text;
   //   const raisesParser = new FunctionCommentRaisesParser();
   //   raisesParser.setStartScope(commentText![14]);
 
@@ -217,8 +247,8 @@ suite("function-comment: constructor: raises", () => {
   //     "../../../../../testContracts/ERC20Compliant/ERC20.cairo"
   //   );
   //   const text = fs.readFileSync(pathFile, "utf8");
-  //   const functionCommentScope = CairoParser.parseFunctionScope(text, "constructor");
-  //   const commentText = CairoParser.parseCommentLines(functionCommentScope![0])!.text;
+  //   const functionScopes = CairoParser.parseFunctionScopeWithMatchAll(text, "constructor");
+  //   const commentText = CairoParser.parseCommentLinesWithMatchAll(functionScopes![0])!.text;
   //   const raisesParser = new FunctionCommentRaisesParser();
 
   //   const targetLineParsing = [
