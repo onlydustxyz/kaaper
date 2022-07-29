5f48353c47f8c1eb2756e578479695ebbe8b888f
jedi <amajidsinar@gmail.com>
2022-07-29 23:47:42 +0100

    test(getScopeParsingResult): should get ERC20_balances in storage_var function scope

--- src/test/suite/function-scope/storage_var.test.ts
+++ src/test/suite/function-scope/storage_var.test.ts
@@ -412,211 +412,297 @@ suite("getScopeParsingResult: storage_var", () => {
     assert.deepEqual(textTarget, commentParsingResult, "failed to parse");
   });
 
-  // test("3", () => {
-  //   const pathFile = path.resolve(
-  //     __dirname,
-  //     "../../../../testContracts/ERC20Compliant/library.cairo"
-  //   );
-  //   const text = fs.readFileSync(pathFile, "utf8");
+  test("should get `ERC20_total_supply` function scope", () => {
+    const pathFile = path.resolve(
+      __dirname,
+      "../../../../testContracts/ERC20Compliant/library.cairo"
+    );
+    const text = fs.readFileSync(pathFile, "utf8");
 
-  //   // parse whole scope
-  //   const functionScopeLines = CairoParser.parseFunctionScope(
-  //     text,
-  //     "storage_var"
-  //   );
+    // parse whole scope
+    const functionScopeLines = CairoParser.parseFunctionScope(
+      text,
+      "storage_var"
+    );
 
-  //   // Function signature parsing
-  //   const functionSignatureParser = new FunctionSignatureRegexParser();
+    // Function signature parsing
+    const functionSignatureParser = new FunctionSignatureRegexParser();
 
-  //   // Comment parsing
-  //   // parse comment lines
-  //   const scopeNumber = 3;
-  //   const functionCommentScope = CairoParser.parseCommentLines(
-  //     functionScopeLines![scopeNumber]
-  //   )!.text;
+    // Comment parsing
+    // parse comment lines
+    const scopeNumber = 3;
+    const functionCommentScope = CairoParser.parseCommentLines(
+      functionScopeLines![scopeNumber]
+    )!;
 
-  //   const functionCommentDescParser = new FunctionCommentDescParser();
-  //   const functionCommentImplicitArgsParser =
-  //     new FunctionCommentImplicitArgsParser();
-  //   const functionCommentExplicitArgsParser =
-  //     new FunctionCommentExplicitArgsParser();
-  //   const functionCommentReturnsParser = new FunctionCommentReturnsParser();
-  //   const functionCommentRaisesParser = new FunctionCommentRaisesParser();
+    const functionCommentText: string = functionCommentScope!.text.join("");
 
-  //   const parsingTarget = [
-  //     {
-  //       attributeName: "storage_var",
-  //       functionName: "ERC20_total_supply",
-  //       functionSignature: {
-  //         implicitArgs: null,
-  //         explicitArgs: null,
-  //         returns: [{ name: "total_supply", type: "Uint256" }],
-  //       },
-  //       functionComment: {
-  //         desc: [
-  //           {
-  //             name: "",
-  //             type: "",
-  //             desc: "Returns total amount of tokens in existence",
-  //           },
-  //         ],
-  //         implicitArgs: null,
-  //         explicitArgs: null,
-  //         returns: [
-  //           {
-  //             name: "total_supply",
-  //             type: "Uint256",
-  //             desc: "The total amount of tokens in existence",
-  //           },
-  //         ],
-  //         raises: null,
-  //       },
-  //     },
-  //   ];
+    const functionCommentDescParser = new FunctionCommentDescParser(
+      functionCommentText
+    );
+    const functionCommentImplicitArgsParser =
+      new FunctionCommentImplicitArgsParser(functionCommentText);
+    const functionCommentExplicitArgsParser =
+      new FunctionCommentExplicitArgsParser(functionCommentText);
+    const functionCommentReturnsParser = new FunctionCommentReturnsParser(
+      functionCommentText
+    );
+    const functionCommentRaisesParser = new FunctionCommentRaisesParser(
+      functionCommentText
+    );
 
-  //   var parsingOutput = [
-  //     {
-  //       attributeName: functionSignatureParser.getAttributeName(
-  //         functionScopeLines![scopeNumber].text
-  //       ),
-  //       functionName: functionSignatureParser.getFunctionName(
-  //         functionScopeLines![scopeNumber].text
-  //       ),
-  //       functionSignature: {
-  //         implicitArgs: functionSignatureParser.getImplicitArgs(
-  //           functionScopeLines![scopeNumber].text
-  //         ),
-  //         explicitArgs: functionSignatureParser.getExplicitArgs(
-  //           functionScopeLines![scopeNumber].text
-  //         ),
-  //         returns: functionSignatureParser.getReturns(
-  //           functionScopeLines![scopeNumber].text
-  //         ),
-  //       },
-  //       functionComment: {
-  //         desc: functionCommentDescParser.parseCommentLines(functionCommentScope!),
-  //         implicitArgs: functionCommentImplicitArgsParser.parseCommentLines(
-  //           functionCommentScope!
-  //         ),
-  //         explicitArgs: functionCommentExplicitArgsParser.parseCommentLines(
-  //           functionCommentScope!
-  //         ),
-  //         returns: functionCommentReturnsParser.parseCommentLines(
-  //           functionCommentScope!
-  //         ),
-  //         raises: functionCommentRaisesParser.parseCommentLines(functionCommentScope!),
-  //       },
-  //     },
-  //   ];
+    const parsingTarget = [
+      {
+        attributeName: "storage_var",
+        functionName: "ERC20_total_supply",
+        functionSignature: {
+          implicitArgs: null,
+          explicitArgs: null,
+          returns: [{ name: "total_supply", type: "Uint256" }],
+        },
+        functionComment: {
+          desc: [
+            {
+              name: "",
+              type: "",
+              desc: "Returns total amount of tokens in existence",
+            },
+          ],
+          implicitArgs: null,
+          explicitArgs: null,
+          returns: [
+            {
+              name: "total_supply",
+              type: "Uint256",
+              desc: "The total amount of tokens in existence",
+            },
+          ],
+          raises: null,
+        },
+      },
+    ];
 
-  //   assert.deepEqual(parsingTarget, parsingOutput, "failed to parse");
-  // });
+    var parsingOutput = [
+      {
+        attributeName: functionSignatureParser.getAttributeName(
+          functionScopeLines![scopeNumber].text
+        ),
+        functionName: functionSignatureParser.getFunctionName(
+          functionScopeLines![scopeNumber].text
+        ),
+        functionSignature: {
+          implicitArgs: functionSignatureParser.getImplicitArgs(
+            functionScopeLines![scopeNumber].text
+          ),
+          explicitArgs: functionSignatureParser.getExplicitArgs(
+            functionScopeLines![scopeNumber].text
+          ),
+          returns: functionSignatureParser.getReturns(
+            functionScopeLines![scopeNumber].text
+          ),
+        },
+        functionComment: {
+          desc: functionCommentDescParser.parseCommentLines(
+            functionCommentScope!.text
+          ),
+          implicitArgs: functionCommentImplicitArgsParser.parseCommentLines(
+            functionCommentScope!.text
+          ),
+          explicitArgs: functionCommentExplicitArgsParser.parseCommentLines(
+            functionCommentScope!.text
+          ),
+          returns: functionCommentReturnsParser.parseCommentLines(
+            functionCommentScope!.text
+          ),
+          raises: functionCommentRaisesParser.parseCommentLines(
+            functionCommentScope!.text
+          ),
+        },
+      },
+    ];
 
-  // test("4", () => {
-  //   const pathFile = path.resolve(
-  //     __dirname,
-  //     "../../../../testContracts/ERC20Compliant/library.cairo"
-  //   );
-  //   const text = fs.readFileSync(pathFile, "utf8");
+    // assert.deepEqual(parsingTarget, parsingOutput, "failed to parse");
 
-  //   // parse whole scope
-  //   const functionScopeLines = CairoParser.parseFunctionScope(
-  //     text,
-  //     "storage_var"
-  //   );
+    var commentParsingResult = [];
 
-  //   // Function signature parsing
-  //   const functionSignatureParser = new FunctionSignatureRegexParser();
+    for (let [key, values] of Object.entries(
+      parsingOutput[0].functionComment
+    )) {
+      if (values) {
+        for (const value of values) {
+          const charIndex = value.charIndex;
+          var char = "";
+          for (
+            let i = functionCommentScope!.start + charIndex.start;
+            i < functionCommentScope!.start + charIndex.end;
+            i++
+          ) {
+            char += text.at(i);
+          }
+          const commentParsing = {
+            [key]: char,
+          };
+          commentParsingResult.push(commentParsing);
+        }
+      }
+    }
+    const textTarget = [
+      { desc: "Returns total amount of tokens in existence" },
+      {
+        returns:
+          "total_supply(Uint256): The total amount of tokens in existence",
+      },
+    ];
+    assert.deepEqual(textTarget, commentParsingResult, "failed to parse");
+  });
 
-  //   // Comment parsing
-  //   // parse comment lines
-  //   const scopeNumber = 4;
-  //   const functionCommentScope = CairoParser.parseCommentLines(
-  //     functionScopeLines![scopeNumber]
-  //   )!.text;
+  test("should get `ERC20_balances` function scope", () => {
+    const pathFile = path.resolve(
+      __dirname,
+      "../../../../testContracts/ERC20Compliant/library.cairo"
+    );
+    const text = fs.readFileSync(pathFile, "utf8");
 
-  //   const functionCommentDescParser = new FunctionCommentDescParser();
-  //   const functionCommentImplicitArgsParser =
-  //     new FunctionCommentImplicitArgsParser();
-  //   const functionCommentExplicitArgsParser =
-  //     new FunctionCommentExplicitArgsParser();
-  //   const functionCommentReturnsParser = new FunctionCommentReturnsParser();
-  //   const functionCommentRaisesParser = new FunctionCommentRaisesParser();
+    // parse whole scope
+    const functionScopeLines = CairoParser.parseFunctionScope(
+      text,
+      "storage_var"
+    );
 
-  //   const parsingTarget = [
-  //     {
-  //       attributeName: "storage_var",
-  //       functionName: "ERC20_balances",
-  //       functionSignature: {
-  //         implicitArgs: null,
-  //         explicitArgs: [{ name: "account", type: "felt" }],
-  //         returns: [{ name: "balance", type: "Uint256" }],
-  //       },
-  //       functionComment: {
-  //         desc: [
-  //           {
-  //             name: "",
-  //             type: "",
-  //             desc: "Returns the amount of tokens owned by an account",
-  //           },
-  //         ],
-  //         implicitArgs: null,
-  //         explicitArgs: [
-  //           {
-  //             name: "account",
-  //             type: "felt",
-  //             desc: "The address of the account",
-  //           },
-  //         ],
-  //         returns: [
-  //           {
-  //             name: "balance",
-  //             type: "Uint256",
-  //             desc: "The amount of tokens owned by an account",
-  //           },
-  //         ],
-  //         raises: null,
-  //       },
-  //     },
-  //   ];
+    // Function signature parsing
+    const functionSignatureParser = new FunctionSignatureRegexParser();
 
-  //   var parsingOutput = [
-  //     {
-  //       attributeName: functionSignatureParser.getAttributeName(
-  //         functionScopeLines![scopeNumber].text
-  //       ),
-  //       functionName: functionSignatureParser.getFunctionName(
-  //         functionScopeLines![scopeNumber].text
-  //       ),
-  //       functionSignature: {
-  //         implicitArgs: functionSignatureParser.getImplicitArgs(
-  //           functionScopeLines![scopeNumber].text
-  //         ),
-  //         explicitArgs: functionSignatureParser.getExplicitArgs(
-  //           functionScopeLines![scopeNumber].text
-  //         ),
-  //         returns: functionSignatureParser.getReturns(
-  //           functionScopeLines![scopeNumber].text
-  //         ),
-  //       },
-  //       functionComment: {
-  //         desc: functionCommentDescParser.parseCommentLines(functionCommentScope!),
-  //         implicitArgs: functionCommentImplicitArgsParser.parseCommentLines(
-  //           functionCommentScope!
-  //         ),
-  //         explicitArgs: functionCommentExplicitArgsParser.parseCommentLines(
-  //           functionCommentScope!
-  //         ),
-  //         returns: functionCommentReturnsParser.parseCommentLines(
-  //           functionCommentScope!
-  //         ),
-  //         raises: functionCommentRaisesParser.parseCommentLines(functionCommentScope!),
-  //       },
-  //     },
-  //   ];
+    // Comment parsing
+    // parse comment lines
+    const scopeNumber = 4;
+    const functionCommentScope = CairoParser.parseCommentLines(
+      functionScopeLines![scopeNumber]
+    )!;
 
-  //   assert.deepEqual(parsingTarget, parsingOutput, "failed to parse");
-  // });
+    const functionCommentText: string = functionCommentScope!.text.join("");
+
+    const functionCommentDescParser = new FunctionCommentDescParser(
+      functionCommentText
+    );
+    const functionCommentImplicitArgsParser =
+      new FunctionCommentImplicitArgsParser(functionCommentText);
+    const functionCommentExplicitArgsParser =
+      new FunctionCommentExplicitArgsParser(functionCommentText);
+    const functionCommentReturnsParser = new FunctionCommentReturnsParser(
+      functionCommentText
+    );
+    const functionCommentRaisesParser = new FunctionCommentRaisesParser(
+      functionCommentText
+    );
+
+    const parsingTarget = [
+      {
+        attributeName: "storage_var",
+        functionName: "ERC20_balances",
+        functionSignature: {
+          implicitArgs: null,
+          explicitArgs: [{ name: "account", type: "felt" }],
+          returns: [{ name: "balance", type: "Uint256" }],
+        },
+        functionComment: {
+          desc: [
+            {
+              name: "",
+              type: "",
+              desc: "Returns the amount of tokens owned by an account",
+            },
+          ],
+          implicitArgs: null,
+          explicitArgs: [
+            {
+              name: "account",
+              type: "felt",
+              desc: "The address of the account",
+            },
+          ],
+          returns: [
+            {
+              name: "balance",
+              type: "Uint256",
+              desc: "The amount of tokens owned by an account",
+            },
+          ],
+          raises: null,
+        },
+      },
+    ];
+
+    var parsingOutput = [
+      {
+        attributeName: functionSignatureParser.getAttributeName(
+          functionScopeLines![scopeNumber].text
+        ),
+        functionName: functionSignatureParser.getFunctionName(
+          functionScopeLines![scopeNumber].text
+        ),
+        functionSignature: {
+          implicitArgs: functionSignatureParser.getImplicitArgs(
+            functionScopeLines![scopeNumber].text
+          ),
+          explicitArgs: functionSignatureParser.getExplicitArgs(
+            functionScopeLines![scopeNumber].text
+          ),
+          returns: functionSignatureParser.getReturns(
+            functionScopeLines![scopeNumber].text
+          ),
+        },
+        functionComment: {
+          desc: functionCommentDescParser.parseCommentLines(
+            functionCommentScope!.text
+          ),
+          implicitArgs: functionCommentImplicitArgsParser.parseCommentLines(
+            functionCommentScope!.text
+          ),
+          explicitArgs: functionCommentExplicitArgsParser.parseCommentLines(
+            functionCommentScope!.text
+          ),
+          returns: functionCommentReturnsParser.parseCommentLines(
+            functionCommentScope!.text
+          ),
+          raises: functionCommentRaisesParser.parseCommentLines(
+            functionCommentScope!.text
+          ),
+        },
+      },
+    ];
+
+    // assert.deepEqual(parsingTarget, parsingOutput, "failed to parse");
+
+    var commentParsingResult = [];
+
+    for (let [key, values] of Object.entries(
+      parsingOutput[0].functionComment
+    )) {
+      if (values) {
+        for (const value of values) {
+          const charIndex = value.charIndex;
+          var char = "";
+          for (
+            let i = functionCommentScope!.start + charIndex.start;
+            i < functionCommentScope!.start + charIndex.end;
+            i++
+          ) {
+            char += text.at(i);
+          }
+          const commentParsing = {
+            [key]: char,
+          };
+          commentParsingResult.push(commentParsing);
+        }
+      }
+    }
+    const textTarget = [
+      { desc: "Returns the amount of tokens owned by an account" },
+      { explicitArgs: "account(felt): The address of the account" },
+      { returns: "balance(Uint256): The amount of tokens owned by an account" },
+    ];
+    assert.deepEqual(textTarget, commentParsingResult, "failed to parse");
+  });
 
   // test("5", () => {
   //   const pathFile = path.resolve(
