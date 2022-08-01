e17174cb867fec79e87bf2c781ca2547cac0f4be
jedi <amajidsinar@gmail.com>
2022-08-01 20:00:59 +0100

    docs(FunctionCommentDescParser): add comment

--- src/lib/parser/function-comment/desc.ts
+++ src/lib/parser/function-comment/desc.ts
@@ -11,6 +11,7 @@ export default class FunctionCommentDescParser extends BaseCommentParser {
     const lineCommentInsideScope = this.isInsideScope(line, this.regex);
     if (lineCommentInsideScope !== null) {
       const startLineIndex = lineCommentInsideScope.index!;
+      // startLineIndex + 1 because the startline would be the the space after the #(1 character)
       const startDescIndex =
         startLineIndex + 1 + lineCommentInsideScope[1].length;
       const desc = lineCommentInsideScope[2];
